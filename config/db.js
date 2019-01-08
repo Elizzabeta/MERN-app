1e1de9232f624ba1b63c25f1e725e5780dfe0a95
Elizzabeta <elizabetajovanovska48@gmail.com>
2019-01-08 20:54:33 +0100

    Backend setup

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbURI: 'mongodb://admin:adminadmin9@ds149414.mlab.com:49414/mern_shopping'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/db').dbURI;
+
+//Connect to DB
+mongoose.connect(db)
+    .then(() => console.log('DB Connected...'))
+    .catch(err => console.log(err));
+
+app.use('/api/items', items);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
